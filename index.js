643fa6de4db8f0bf6faecdd08496cb4176b0f947
Nasser Sanou <nassersanou23@gmail.com>
2018-09-15 03:58:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,15 +3,18 @@
 function happyHolidays() {
   return "Happy holidays!"
 }
+
 // 2
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
 
+// 3
 function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
+// 4
 function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`
 }

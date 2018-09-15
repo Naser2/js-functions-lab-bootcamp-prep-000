a5961bf256bae109fbbec4acd423bfe75ea22164
Nasser Sanou <nassersanou23@gmail.com>
2018-09-15 03:58:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,23 +5,18 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 
-// write your code below!
-// 1
-function happyHolidays() {
-  return "Happy holidays!"
-}
-
-// 2
+// 2 
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
 
+
 // 3
 function happyHolidayTo(holiday, name) {
-  return `Happy ${holiday}, ${name}!`
+return `Happy ${holiday}, ${name} `
 }
 
 // 4
 function holidayCountdown(holiday, days) {
-  return `It's ${days} days until ${holiday}!`
+  return "It's ${days} days until${holiday}!"
 }
